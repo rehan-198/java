3b41edff907bb9345dd7bb7771b4bd0f187f0e49
Rehan Aslam <rehanaslam198@gmail.com>
2025-05-20 00:47:37 +0530

    Add files via upload

---
+++ arithmeticOperator.java
@@ -0,0 +1,14 @@
+public class arithmeticOperator { 
+    public static void main(String args[]){
+        int A=10;
+        int B=5;
+        System.out.println("Add=" + (A+B));
+        System.out.println("Sub=" + (A-B));
+        System.out.println("Mul=" + (A*B));
+        System.out.println("Divide=" + (A/B));
+        System.out.println("Modulo=" + (A%B));
+
+
+    }
+    
+}
